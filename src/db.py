c2656c0d13172ff50091f1908b00172f430527e6
ashliezhang <janexu770@gmail.com>
2024-11-26 23:15:22 -0500

    Implement user and post models

--- src/db.py
+++ src/db.py
@@ -9,6 +9,30 @@ class User(db.Model):
   User Model
   """
   __tablename__ = "users"
+  id = db.Column(db.Integer, primary_key=True, autoincrement=True)
+  username = db.Column(db.String, nullable=False)
+  email = db.Column(db.String, nullable=False)
+  password = db.Column(db.String, nullable=False)
+  posts = db.relationship("Post", back_populates="users", cascade="delete")
+  recipes = db.relationship("Recipe", back_populates="users", cascade="delete")
+  
+  
+  def __init__(self, **kwargs):
+    """
+    Initialize User object/entry
+    """
+    self.username = kwargs.get("username", "")
+    self.email = kwargs.get("email", "")
+    self.password = kwargs.get("password", "")
+
+  def serialize(self):
+    """ 
+    Serialize a user object
+    """
+    return {
+      "id": self.id,
+      "username": self.username
+    }
 
 
 class Post(db.Model): 
@@ -16,6 +40,39 @@ class Post(db.Model):
   Post Model
   """
   __tablename__ = "posts"
+  id = db.Column(db.Integer, primary_key=True, autoincrement=True)
+  user_id = db.Column(db.Integer, db.ForeignKey("users.id"), nullable=False)
+  recipe_id = db.Column(db.Integer, db.ForeignKey("recipes.id"), nullable=True)
+  image_url = db.Column(db.String, nullable=False)
+  title = db.Column(db.String, nullable=False)
+  caption = db.Column(db.String, nullable=True)
+  created_at = db.Column(db.DateTime, nullable=False)
+
+  def __init__(self, **kwargs):
+    """
+    Initialize Post object/entry
+    """
+    self.user_id = kwargs.get("user_id", "")
+    self.recipe_id = kwargs.get("recipe_id")
+    self.image_url = kwargs.get("image_url", "")
+    self.title = kwargs.get("title", "")
+    self.caption = kwargs.get("caption", "")
+    self.created_at = kwargs.get("created_at")
+
+  def serialize(self):
+    """ 
+    Serialize a post object
+    """
+    return {
+      "id": self.id,
+      "user_id": self.user_id,
+      "recipe_id": self.recipe_id,
+      "image_url": self.image_url,
+      "title": self.title,
+      "caption": self.caption,
+      "created_at": self.created_at
+    }
+
 
 class MealTypeEnum(enum.Enum):
   breakfast = "breakfast"
@@ -34,15 +91,19 @@ class Recipe(db.Model):
   description = db.Column(db.String, nullable=False) 
   instructions = db.Column(db.String, nullable=False)
   meal_type = db.Column(db.Enum(MealTypeEnum), nullable=True)
+  user_id = db.Column(db.Integer, db.ForeignKey("users.id"), nullable=False)
+  post_id = db.Column(db.Integer, db.ForeignKey("posts.id"), nullable=False)
 
   def __init__(self, **kwargs):
     """
     Initialize Recipe object/entry
     """
     self.title = kwargs.get("title", "")
-    self.done = kwargs.get("description", "")
+    self.description = kwargs.get("description", "")
     self.instructions = kwargs.get("instructions", "")
     self.meal_type = kwargs.get("meal_type", MealTypeEnum.any)
+    self.user_id = kwargs.get("user_id", "")
+    self.post_id = kwargs.get("post_id", "")
   
   def serialize(self):
     """ 
